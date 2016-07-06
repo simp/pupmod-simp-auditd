76f892ca5580dc7e6c9e19c3942fdb3dfd32d081
NickMarkowski <nmarkowski@mister.noodle>
2016-07-06 14:10:35 -0400

    (SIMP-1194) Added audit rule for renameat.
    - Added a default audit rule for 'renameat', per CCE-26651-0.
    
    (SIMP-1185) Fixed action list validation.
    
    - Added an auditd_version fact.
    - Updated validation for *_action lists to differentiate between
      auditd versions.
    
    SIMP-1194 #close
    SIMP-1185 #close
    SIMP-1201 #close
    
    Change-Id: I6ee0734cabd009e83e6fe3769215a2de8e7b0fd6

---
+++ lib/facter/auditd_version.rb
@@ -0,0 +1,11 @@
+# _Description_
+#
+# Set a fact to return the version of auditd that is installed.
+# This is useful for applying the correct configuration file options.
+#
+Facter.add("auditd_version") do
+  confine { File.exist?('/sbin/auditctl') && File.executable?("/sbin/auditctl") }
+  setcode do
+    %x{/sbin/auditctl --version}.chomp.split(/\s+/)[-1]
+  end
+end
