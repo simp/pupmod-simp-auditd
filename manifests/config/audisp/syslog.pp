ce24a1a96e06b3a9256892ceab3886326f1d864f
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2016-09-21 19:09:33 -0400

    (SIMP-1529) add priotiy and facity options to syslog.conf file for audisp.    added the options to class auidt:config:audisp:syslog. SIMP-1529 #close

--- manifests/config/audisp/syslog.pp
+++ manifests/config/audisp/syslog.pp
@@ -19,10 +19,28 @@
 # [*encrypted_port*]
 #   The port to which to send the logs on localhost.
 #
+# [*priority*]
+#   The priority of the messages you want forwarded
+#   This value is used in the 
+#   audisp/plugins.d/syslog.conf file
+#   default: LOG_INFO
+#
+# [*facility*] 
+#   The log file facility you want used to forwarded the messages to.
+#   This value is used in the 
+#   audisp/plugins.d/syslog.conf file
+#   default: "" 
+#     the version of audit used does not recognize LOG_USER but
+#     but sets facility to LOG_USER if the entry is "".
+#
 class auditd::config::audisp::syslog (
+  $priority = "LOG_INFO",
+  $facility  = "LOG_USER",
   $log_servers = hiera('log_servers',[])
 ) {
   validate_array($log_servers)
+  validate_array_member($priority, ['LOG_INFO', 'LOG_DEBUG', 'LOG_NOTICE', 'LOG_WARNING', 'LOG_ERR', 'LOG_CRIT', 'LOG_ALERT', 'LOG_EMERG'])
+  validate_array_member($facility, ['LOG_AUTH', 'LOG_AUTHPRIV', 'LOG_CRON', 'LOG_DAEMON', 'LOG_KERN', 'LOG_LP', 'LOG_MAIL', 'LOG_NEWS', 'LOG_SYSLOG', '', 'LOG_UUCP', 'LOG_LOCAL0', 'LOG_LOCAL1','LOG_LOCAL2','LOG_LOCAL3','LOG_LOCAL4','LOG_LOCAL5','LOG_LOCAL6','LOG_LOCAL7', ])
 
   if !empty($log_servers) {
     # Note: This only happens if you are offloading your logs.
@@ -38,7 +56,7 @@ active = yes
 direction = out
 path = builtin_syslog
 type = builtin
-args = LOG_INFO
+args = ${priority} ${facility}
 format = string
 "
   }
