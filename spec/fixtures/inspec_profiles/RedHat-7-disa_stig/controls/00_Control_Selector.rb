9e909776f8d6a25aa8db485f949de942c6525605
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-06-04 11:31:49 -0400

    (SIMP-4679) Update compliance tests (#71)
    * Updated the tests to match the new patterns and work with the latest version of inspec
    * Requires simp-beaker-helpers 1.10.9
    * Add missing systemd dependency to fixtures

--- spec/fixtures/inspec_profiles/RedHat-7-disa_stig/controls/00_Control_Selector.rb
+++ spec/fixtures/inspec_profiles/RedHat-7-disa_stig/controls/00_Control_Selector.rb
@@ -1,22 +1,37 @@
+skips = {}
 overrides = []
+subsystems = [ 'audit' ]
 
 require_controls 'disa_stig-el7-baseline' do
-  # SIMP uses Rsyslog for log offloading
-  # Need to create a profile and tests
-  control 'V-72087' do
-    overrides << self.to_s
-
-    only_if { file('/etc/audisp/audisp-remote.conf').exist? }
-  end
-
-  @conf['profile'].info[:controls].each do |ctrl|
-    next if overrides.include?(ctrl[:id])
-
-    tags = ctrl[:tags]
-    if tags && tags[:subsystems]
-      if tags[:subsystems].include?('audit')
-        control ctrl[:id]
+  skips.each_pair do |ctrl, reason|
+    control ctrl do
+      describe "Skip #{ctrl}" do
+        skip "Reason: #{skips[ctrl]}" do
+        end
       end
     end
   end
+
+  @conf['profile'].info[:controls].each do |ctrl|
+    next if (overrides + skips.keys).include?(ctrl[:id])
+
+    tags = ctrl[:tags]
+    if tags && tags[:subsystems]
+      subsystems.each do |subsystem|
+        if tags[:subsystems].include?(subsystem)
+          control ctrl[:id]
+        end
+      end
+    end
+  end
+
+  ## Overrides ##
+
+# # USEFUL DESCRIPTION
+# control 'V-IDENTIFIER' do
+#   # Enhancement, leave this out if you just want to add a different test
+#   overrides << self.to_s
+#
+#   only_if { file('whatever').exist? }
+# end
 end
