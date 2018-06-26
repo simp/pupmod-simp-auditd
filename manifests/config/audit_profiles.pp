a17d74bca9f84d62b933466800646629e2fd0e43
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2018-06-26 12:36:36 -0400

    (SIMP-4747) Allow multiple profs & add stig prof (#77)
    - Added ability to select one or more audit profiles.  When multiple
      profiles are selected, their rules are effectively concatenated in
      the order in which the profiles are listed in
      auditd::default_audit_profiles.
    - The following API Changes were made in support of multiple audit
      profiles:
      - auditd::config and auditd::config::audit_profiles::simp are now
        private.
      - `auditcl` global configuration options that were in
        auditd::config::audit_profiles::simp are now in auditd, instead.
        They were moved because they are now applied to the set of audit
        profiles selected, not just the 'simp' audit profile.
      - Global audit rule configuration has been moved from
        auditd::config::audit_profiles::simp to a new class,
        auditd::config::audit_profiles.
      - Some previously hard-coded, internal configuration is now exposed
        as data-in-modules.
      - Some configuration parameter names have been changed for clarity.
    - Added 'stig' audit profile which manages rules that match DISA STIG
      checks, exactly.
      - For executables explicitly listed in the RHEL7 STIG, includes watchs
        for binaries in the real paths (/usr/bin, /usr/sbin) and linked paths
        (/bin, /sbin). This is to address inconsistencies among the STIG and
        the Inspec and OSCAP scans. (All should use the real paths, but don't.)
    - 'simp' profile changes:
      - Fixed umask syscall rules.  These rules require arch filters.
      - Fixed clock_settime syscall rules.  Per the sample STIG audit rules
        packaged in the auditd RPM, these rules require an 'a0' filter.
      - Fixed bug in which /var/log/tallylog was grouped with session
        instead of logins.
      - Fixed bug in which the /etc/pam.d watch rule had the wrong tag
      - Added missing syscalls to su-root basic, aggressive, and insane
        syscall lists
      - Added an option to audit the execution of rename/remove operations
        for non-service users (rename', 'renameat', rmdir', 'unlink', and
        'unlinkat', for DISA STIG)
      - Added watch rules for /etc/hostname and /etc/NetworkManager (for
        centos7) pulled from the sample STIG audit rules packaged in the
        auditd RPM.
      - For executables explicitly listed in the RHEL7 STIG, includes watchs
        for binaries in the real paths (/usr/bin, /usr/sbin) and linked paths
        (/bin, /sbin). This is to address inconsistencies among the STIG and
        the Inspec and OSCAP scans. (All should use the real paths, but don't.)
    
    - Other changes
      - Replaced params.pp with simplib::assert_metadata
      - Moved some of the logic buried in templates into manifests and/or
        data-in-modules
      - Switched a few templates to epp format
      - Beefed up class documentation
      - Removed outdated info that should be in REFERENCE.md from README.md
      - Tests, tests, and more tests
    
    SIMP-4747 #close
    SIMP-4748 #close

---
+++ manifests/config/audit_profiles.pp
@@ -0,0 +1,77 @@
+# This class is called from auditd::config.  It provides global audit
+# rule configuration and a base set of audit rules based on the
+# built-in audit profile(s).
+#
+# The configuration generated is contained in a set of files in
+# `/etc/audit/rules.d`, which `augenrules` parses for `auditd` in
+# natural sort order, to create a single `/etc/audit/auditd.rules`
+# file. The generated files are as follows:
+# - `00_head.rules`:  Contains `auditctl` general configuration to
+#   remove existing rules when the rules are reloaded, ignore rule
+#   load errors/failures, and set the buffer size, failure mode,
+#   and rate limiting.
+# - `05_default_drop.rules`: Contains filtering rules for efficiency
+#   - Rules to drop prolific events of low-utility
+#   - Rules to restrict events based on `auid` constraints that would
+#     normally be applied to all rules
+# - `50_*base.rules`:
+#   - Nominal base rules for one or more built-in profiles.
+#   - One file will exist for each desired, built-in profile.
+#   - Files are named so that the ordering of profiles listed
+#     in `$::auditd::default_audit_profiles` is preserved.
+#   - The corresponding class for each profile is
+#    `auditd::config::audit_profiles::<profile name>`.
+# - `75.init.d_auditd.rules`:
+#    - A watch rule for `/etc/rc.d/init.d/auditd` permissions changes
+#    - A watch rule for permissions changes to the `auditd` log file
+# - `75.rotated_audit_logs.rules`
+#    - Watch rules for permissions changes to the rotated `auditd` log files
+# - `99_tail.rules`
+#   - `auditctl` immutable option, when `$::auditd::immutable` is 'true'
+#
+class auditd::config::audit_profiles {
+
+  assert_private()
+
+  $_common_template_path = "${module_name}/rule_profiles/common"
+
+  auditd::rule { 'init.d_auditd':
+    content => "-w /etc/rc.d/init.d/auditd -p wa -k auditd
+                -w ${$::auditd::log_file} -p wa -k audit-logs"
+  }
+
+
+  $_num_logs = $::auditd::num_logs
+  $_log_file = $::auditd::log_file
+  auditd::rule { 'rotated_audit_logs':
+    content => template("${_common_template_path}/rotated_audit_logs.erb")
+  }
+
+  if ( $::auditd::root_audit_level == 'aggressive' ) and ( $::auditd::buffer_size < 32788 ) {
+    $_buffer_size = 32788
+  } elsif ( $::auditd::root_audit_level == 'insane' ) and ( $::auditd::buffer_size < 65576 ) {
+    $_buffer_size = 65576
+  } else {
+    $_buffer_size = $::auditd::buffer_size
+  }
+
+  file { '/etc/audit/rules.d/00_head.rules':
+    content => epp("${_common_template_path}/head.epp")
+  }
+
+  file { '/etc/audit/rules.d/05_default_drop.rules':
+    content => epp("${_common_template_path}/default_drop.epp")
+  }
+
+  file { '/etc/audit/rules.d/99_tail.rules':
+    content => epp("${_common_template_path}/tail.epp")
+  }
+
+  $::auditd::config::profiles.each | String $audit_profile | {
+    # use contain instead of include so that config file changes can
+    # notify auditd::service class
+    contain "auditd::config::audit_profiles::${audit_profile}"
+  }
+
+
+}
