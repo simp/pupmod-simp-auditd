d334e63f10ef3c0fa7155eddfd9bc3d0dfbab3ba
Brandon Ess <brandon.ess@gmail.com>
2018-07-31 05:43:49 -0700

    resolved an issue where Puppet would clobber the log_group each run (#79)
    * resolved an issue where Puppet would clobber the log_group each run

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -5,10 +5,10 @@ require 'spec_helper'
 # class described needs to be the class instantiated, i.e., auditd.
 describe 'auditd' do
   context 'supported operating systems' do
-    on_supported_os.each do |os, facts|
+    on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) do
-          facts
+          os_facts
         end
 
         context "with default parameters" do
@@ -20,7 +20,7 @@ describe 'auditd' do
               :ensure  => 'directory',
               :owner   => 'root',
               :group   => 'root',
-              :mode    => '0640',
+              :mode    => '0600',
               :recurse => true,
               :purge   => true
             })
@@ -62,15 +62,64 @@ describe 'auditd' do
             })
           }
 
-          if facts[:os][:release][:major] == '6'
+          if os_facts[:os][:release][:major] == '6'
             it { is_expected.to contain_augeas('auditd/USE_AUGENRULES').with_changes(
               ['set /files/etc/sysconfig/auditd/USE_AUGENRULES yes'])
             }
           else
             it { is_expected.to_not contain_augeas('auditd/USE_AUGENRULES') }
           end
+
           it { is_expected.to contain_class('auditd::config::audit_profiles') }
           it { is_expected.to contain_class('auditd::config::audit_profiles::simp') }
+        end
+
+        context 'with different log_group' do
+          let(:params) {{ log_group: 'rspec' }}
+
+          it { is_expected.to compile.with_all_deps }
+          it {
+            is_expected.to contain_file('/etc/audit/rules.d').with({
+              :ensure  => 'directory',
+              :owner   => 'root',
+              :group   => 'rspec',
+              :mode    => '0640',
+              :recurse => true,
+              :purge   => true
+            })
+          }
+          it {
+            is_expected.to contain_file('/etc/audit/audit.rules').with({
+              :owner => 'root',
+              :group => 'rspec',
+              :mode  => 'o-rwx'
+            })
+          }
+
+          it {
+            is_expected.to contain_file('/etc/audit/auditd.conf').with({
+              :owner => 'root',
+              :group => 'rspec',
+              :mode  => '0640'
+            })
+          }
+
+          it {
+            is_expected.to contain_file('/var/log/audit').with({
+              :ensure => 'directory',
+              :owner  => 'root',
+              :group  => 'rspec',
+              :mode   => 'o-rwx'
+            })
+          }
+
+          it {
+            is_expected.to contain_file('/var/log/audit/audit.log').with({
+              :owner  => 'root',
+              :group  => 'rspec',
+              :mode   => '0640'
+            })
+          }
         end
 
         context 'with empty default_audit_profiles' do
