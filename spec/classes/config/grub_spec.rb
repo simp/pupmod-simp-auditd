3b1aeb9913625ac69d08d948e2a28ef66fddb3ac
Steven Pritchard <steve@sicura.us>
2024-07-03 15:37:48 -0500

    Clean up legacy fact usage

--- spec/classes/config/grub_spec.rb
+++ spec/classes/config/grub_spec.rb
@@ -5,7 +5,7 @@ describe 'auditd::config::grub' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
         let(:facts) do
-          if ['RedHat','CentOS'].include?(facts[:operatingsystem]) && facts[:operatingsystemmajrelease].to_s < '7'
+          if ['RedHat','CentOS'].include?(facts[:os][:name]) && facts[:os][:release][:major].to_s < '7'
             facts[:apache_version] = '2.2'
             facts[:grub_version] = '0.9'
           else
